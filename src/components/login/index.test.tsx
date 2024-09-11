ae400fa28ae371ab0beba397712a35badef3e9b5
Ram Krishna Das <ram.krishna.das@outlook.com>
2024-09-12 01:12:16 +0530

    DOOR-7 Added okta login functionality to frontend

--- src/components/login/index.test.tsx
+++ src/components/login/index.test.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Login from '.';
-import { MemoryRouter } from 'react-router-dom';
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import Login from ".";
 
-test('renders learn react link', () => {
-  render(
-    <MemoryRouter>
-      <Login />
-    </MemoryRouter>);
-  const linkElement = screen.getAllByText(/login/i).length;
-  expect(linkElement).toBeGreaterThan(0);
+const mockLoginWithRedirect = jest.fn();
+
+jest.mock('@auth0/auth0-react', () => ({
+  ...jest.requireActual('@auth0/auth0-react'),
+  useAuth0: () => ({
+    loginWithRedirect: mockLoginWithRedirect
+  }),
+}));
+
+describe("test Login Page", () => {
+  test("renders Login page", () => {
+    render(<Login />);
+    const linkElement = screen.getAllByText(/login/i).length;
+    expect(linkElement).toBeGreaterThan(0);
+  });
+  test("click of login button okta fn should be called", () => {
+    render(<Login />);
+    const loginLink = screen.getAllByText(/Log in/i);
+    fireEvent.click(loginLink[0]);
+    expect(mockLoginWithRedirect).toHaveBeenCalled();
+  });
 });
